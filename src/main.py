8f4b176447dd15f30a544936894d0ddeb7b888c8
rafaelMachado1891 <rafadmn1@gmail.com>
2025-11-25 23:31:55 -0300

    correcao dos metodos das classes

--- src/main.py
+++ src/main.py
@@ -1,40 +1,15 @@
 from utils import CarregarCsv, Conexao_com_Banco
 
-
-arquivo_fluxo = ('C:/Users/rafad/Documents/Repositorios_Git/fluxo_de_caixa/src/data/fluxo_caixa.csv')
-
-titulos = ('C:/Users/PCP/Documents/repositorios_git/projeto_fluxo_caixa/fluxo_de_caixa/src/data/consulta_titulos.csv')
-
 conexao = Conexao_com_Banco()
 engine = conexao.criar_engine()
 
-carregador_fluxo = CarregarCsv(titulos)
+titulos = ('C:/Users/rafad/Documents/Repositorios_Git/fluxo_de_caixa/src/data/consulta_titulos.csv')
 
-tbl_fluxo = carregador_fluxo.carregar_csv()
+carga_fluxo = CarregarCsv(titulos)
 
-# rename_columns_fluxo = {
-#     'codigo': 'id_lancamento',
-#     'tipo': 'tipo',
-#     'Tipo_C': 'tipo_lancamento',
-#     'DataReceb': 'data_recebimento',
-#     'Descricao': 'descricao_do_lancamento',
-#     'complemento': 'origem_lancamento',
-#     'valor': 'valor',
-#     'NumTitulo': 'numero_titulo',
-#     'ordem': 'ordem',
-#     'Instituicao': 'instituicao_de_pagamento',
-#     'ContaContabil': 'conta_contabil',
-#     'ContaContabilDebito': 'conta_contabil_debito',
-#     'Serie': 'serie',
-#     'Codigo_C': 'id_cliente',
-#     'Num_Pag': 'parcela'
-# }
+carga_fluxo.carregar_csv()
 
-# tbl_fluxo = carregador_fluxo.renomear_colunas(rename_columns_fluxo)
-
-
-
-carregador_fluxo.carregar_no_banco(
+carga_fluxo.carregar_no_banco(
     tabela="tbl_fluxo",
     con=engine,
     metodo="replace",
