26460495c84996e23885683e96ed5079319e4331
rafaelMachado1891 <rafadmn1@gmail.com>
2025-12-23 12:43:00 -0300

    feat cinalmente rodou airflow

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,9 +1,9 @@
-from src.utils import CarregarCsv, Conexao_com_Banco
+from src.utils import CarregarCsv, ConexaoComBanco
 import os
 
 
 def rodar_pipeline():
-    conexao = Conexao_com_Banco(conn_id="postgres_fluxo")
+    conexao = ConexaoComBanco(conn_id="postgres_fluxo")
     engine = conexao.criar_engine()
 
     diretorio = os.path.dirname(os.path.abspath(__file__))
@@ -12,20 +12,20 @@ def rodar_pipeline():
         "tbl_fluxo": "consulta_titulos.csv",
         "tbl_plano_contas": "plano_contas.csv",
         "tbl_lancamentos_fluxo": "fluxo_caixa.csv",
-        "tbl_intituicoes": "instituicoes.csv",
-        "tbl_saldo_inicial": "saldo_inicial.csv"
+        "tbl_instituicoes": "instituicoes.csv",
+        "tbl_saldo_inicial": "saldo_inicial.csv",
     }
 
     for tabela, nome_arquivo in arquivos.items():
         caminho = os.path.join(diretorio, "data", nome_arquivo)
+
         carga = CarregarCsv(caminho)
-        carga.processar(
+        carga.carregar_csv()
+        carga.carregar_no_banco(
             tabela=tabela,
-            con=engine,
+            engine=engine,
             metodo="replace",
             index=False
         )
-        
+
         print(f"dados carregados com sucesso na tabela {tabela}")
-        
-        
